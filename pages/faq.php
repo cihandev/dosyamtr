d1d0c083b369016c09c7c783729e6d979a57a0c7
cihandev <44552258+cihandev@users.noreply.github.com>
2018-10-29 01:01:00 +0300

    Add files via upload

---
+++ pages/faq.php
@@ -0,0 +1,45 @@
+<? include ("ads.html") ?>
+<div class=content>
+<p>
+<h1> <?php echo $sitename ?>'ý Kimler Kullanýr? </h1>
+Herkes, Dosya Yüklemek Ýsteyen Herkes. Birçok ülkede hayatýn her kesiminden milyonlarca insan. Öðrenciler, profesyonel iþ insanlarý, anneler, doktorlar, tesisatçýlar, Sigortacý, mortgage brokerlarý, adýný siz koyun.
+<p>
+<h1> <?php echo $sitename ?>'ý Neden Kullanmalýyým? </h1>
+<p>
+Çünkü <?php echo $sitename ?> Dosya Yüklemenin En Etkin, En Uygun ve En Kolay Yoludur. Dosyalarýnýz En Ýyi Þekilde Saklanýr ve En Son Hýzda Eriþime Açýk Tutulur.
+<h1><?php echo $sitename ?> Aþaðýdaki sorunlarý ortadan kaldýrýr: </h1>
+<ul>
+<li> Dosyalarýnýza Eriþememe.  
+<li> Güvensiz ve Kötü Yoldan Dosya Eriþimi.  
+<li> Dosya Eriþimini Yavaþ Yoldan Saðlama.
+</ul>
+<p>
+<h1>Bana Yararlarý Nelerdir?</h1>
+<ul>
+<li> Eðer Birkaç Kiþiye Birden Dosya Eriþimi Saðlamak Ýstiyorsanýz, Dosyalarý Tekrardan Yüklemeniz Gerekmez. Ayný Ýndirme Linkini Birden Fazla Kiþilerlede Paylaþabilirsiniz. 
+<li> Dosyalarýnýz'a Baþkalarý Siz Ýstemediðiniz Sürece Asla Kötü Yoldan Eriþemez.  
+<li> Kesinlikle Ücretsizdir.
+<li> Hiçbir Özel Yazýlým veya Ekipman'a Ýhtiyacýnýz Yoktur.
+<li> Bir Dosya Kaybolmuyor veya Bir Yere Gitmiyor.
+<li> Size Kolay Yoldan Dosya Ýndirimi Saðlar. Yüksek Hýzda ve Güvenli Yoldan Dosyanýza Eriþim Saðlayabilirsiniz.
+<li> Kayýp veya Yanlýþlýkla Silinen Dosyalarýnýzý Yeniden Ýndirebilirsiniz. Bir Nevi Veri Saklamanýza Yarar.
+<li> Dosyalarýnýza Sizin Ýstediðiniz Herkes Eriþir Fakat Silemez veya Kaldýramaz.
+</ul>
+<p>
+<h1>Ne Tür Dosyalar Yüklerim? </h1>
+Müzik videolarý, mp3s, þarký, bilgisayar oyunlarý, bilgisayar yazýlýmý, cep telefonu zil sesleri, dijital kamera fotoðraflarý ve albümleri: bireyler için, bu gibi þeyler olabilir.
+<p>
+Profesyonel için bu olabilir: Excel tablolarý, PowerPoint sunumlarý, veri dosyalarý, Word belgeleri, proje planlarý, ev inþaat mimar çizimler, ses kayýtlarý, satýþ broþür, tamamlanan ev ipotek sözleþmeleri, Týbbi Laboratuvar sonuçlarý, computerserver yapýlandýrma dosyalarý, reklam broþürleri. Gördüðünüz gibi, liste bitmek bilmiyor. Siz hemen her türlü öðeyi ve PC'nizin sabit sürücüsündeki her þeyi yükleyebilirsiniz.
+<p>
+<h1>Dosyalarým Ne Kadar Sýklýkla Ýndirilebilir? <?php echo $sitename ?>? </h1>
+Sýnýr Yoktur! Ýsterseniz 1 Milyondan Fazlada Ýndirebilirsiniz. 
+<p>
+<h1><?php echo $sitename ?> Dosyalarýmý Ne Kadar Süre Sunucularýnda Barýndýrýr?</h1>
+Siz Silmediðiniz, Dosyalarýnýzda Uygunsuz Ýçerik Bulunmadýðý ve Telif Haklarýna Uygun Olduðu Sürece Barýndýrmaktadýr.
+<p>
+<h1>Hangi Dosyalarýn Yüklenmesine Ýzin Verilir? </h1>
+Tüm hizmet açýsýndan veya ilgili ülkelerin yasalarýna aykýrý olmayan dosyalarý. Bu dosyalar telif ihlal veya Çocuk Pornografisi Ýçermediði Sürece Yüklenmesine Ýzin Verilir.
+<p>
+<h1>En Fazla Kaç MB Dosya Yükleyebilirim?</h1>
+Maksimum izin verilen dosya boyutu <?php echo $maxfilesize; ?> megabytesdir.</p>
+</div>
---
+++ pages/tos.php
@@ -0,0 +1,19 @@
+<div class=content>
+<h1><?php echo $sitename ?> Kullaným Koþullarý</h1>
+<p>
+<b>Yükleme Yaparak <?php echo $sitename ?> Koþullarýný Kabul Etmiþ Olacaksýnýz.</b>
+<ul>
+<li>Terör Mesajlarý, Ýþkence veya Ölüm Gösteren ve Ýçeriði Bulunan Dosyalar Yüklemeyeceðim.
+<li>Çocuk Pornografisi Ýçeren Dosyalar Yüklemeyeceðim. 
+<li>Yüklediðim Dosyalardan Tamamiyle Kendim Sorumluyum. <?php echo $sitename ?> Sorumluluðunda Deðildir.
+<li>Telif Hakký Ýhlal Eden Dosyalar Yüklemeyeceðim.
+</ul>
+<p>
+<b>Bu Koþullarý Kabul Ettiðim Taktirde Aykýrý Ýçerik Yüklersem Ne Olur?</b>
+<p>
+<li><?php echo $sitename ?> Tarafýndan Dosyalarýnýza El Koyulur.
+<li><?php echo $sitename ?> Tarafýndan Dosyalarýnýz Silinebilir.
+<li><?php echo $sitename ?> Tarafýndan Dosyalarýnýz Gerekli Makamlara ve Kuruluþlara Ulaþtýrýlabilir.
+<li><?php echo $sitename ?> Tarafýndan Siteden Uzaklaþtýrýlabilirsiniz.
+<li><?php echo $sitename ?> Daha Bir Çok Hakký Kendinde Saklý Tutar veya Tutabilir.
+</div>
